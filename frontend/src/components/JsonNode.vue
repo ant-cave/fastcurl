2696aaf6f5a542c1f7697be91704b58378631155
ant-cave <antmmmm@126.com>
2026-07-28 21:58:33 +0800

    chore: 更新版权声明，添加邮箱和 GitHub 链接

--- frontend/src/components/JsonNode.vue
+++ frontend/src/components/JsonNode.vue
@@ -1,5 +1,6 @@
 <!-- SPDX-License-Identifier: AGPL-3.0-or-later -->
-<!-- Copyright (C) 2026 ant-cave -->
+<!-- Copyright (C) 2026 ant-cave <antmmmmm@outlook.com> -->
+<!-- https://github.com/ant-cave -->
 
 <template>
   <div class="select-none">
